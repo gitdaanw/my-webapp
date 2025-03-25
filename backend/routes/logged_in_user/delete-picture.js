b7306916aa425fd8fec42b2828074a38c458b82e
DaanWeer <daan.weerheim@odido.nl>
2025-03-25 10:21:22 +0100

    adds a working delete picture solution

--- backend/routes/logged_in_user/delete-picture.js
+++ backend/routes/logged_in_user/delete-picture.js
@@ -6,8 +6,8 @@ const router = express.Router();
 // import authentication function
 const {requireLoginPage} = require("../../utils/authentication");
 
-// route PATCH /pictures/id, requirelogin, update existing picture
-router.patch("/:id", requireLoginPage, (req, res) => {
+// route delete /pictures/id, requirelogin, delete picture from collection
+router.delete("/:id", requireLoginPage, (req, res) => {
     // define the filepath to collection
     const filePath = path.join(__dirname, "../../data/picture_collection.json");
 
@@ -21,33 +21,19 @@ router.patch("/:id", requireLoginPage, (req, res) => {
             return res.status(404).json({ message: "Picture not found" });
         }
 
-        // list with fields that are allowed to be updated
-        // as for demo this is blocked in frontend so only description_nl can be updated
-        const allowedFields = [
-            "date",
-            "country_nl", "country_en",
-            "city",
-            "category_nl", "category_en",
-            "description_nl", "description_en",
-            "image"
-        ];
+        // remove the picture from the array
+        data.pictures.splice(index, 1);
 
-        // update the allowed fields
-        for (const key of allowedFields) {
-            if (key in req.body) {
-                data.pictures[index][key] = req.body[key];
-            }
-        }
 
         // write updated data back to the collection
         fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
 
         // send response with success
-        res.status(201).json({message: "Picture updated"});
+        res.status(201).json({message: "Picture deleted"});
 
         // handle errors related to read and write
     } catch (error) {
-        console.error("Error updating picture:", error);
+        console.error("Error deleting picture:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 });
